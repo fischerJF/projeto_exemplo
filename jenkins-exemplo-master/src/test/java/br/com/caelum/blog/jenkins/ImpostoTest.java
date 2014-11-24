729bdd54e6dd680ce48540b82ced18c72d094cde
fischer <fischerbatera@hotmail.com>
2014-11-24 15:27:53 -0200

    segundo commit

--- jenkins-exemplo-master/src/test/java/br/com/caelum/blog/jenkins/ImpostoTest.java
+++ jenkins-exemplo-master/src/test/java/br/com/caelum/blog/jenkins/ImpostoTest.java
@@ -11,7 +11,7 @@ public class ImpostoTest {
 
 	@Test
 	public void deveCalcularImpostoDeDezPorCento() {
-		Orcamento orcamento = new Orcamento(100.0);
+		Orcamento orcamento = new Orcamento(10.0);
 		Imposto imposto = new Imposto();
 		assertEquals(10.0, imposto.calcula(orcamento), Double.MIN_VALUE);
 	}
